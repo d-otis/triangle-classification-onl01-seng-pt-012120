07e014337f691d49f7fb99cee67e01caedff870e
Dan Foley <dan@dan-foley.com>
2020-02-06 21:30:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
   end
   
   def kind
-    if @side1 + @side2 > @side3
+    sides = [@side1, @side2, @side3]
+    if !(@side1 + @side2 > @side3) || 
   end
   
   class TriangleError < StandardError
