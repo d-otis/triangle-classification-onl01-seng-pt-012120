25016af07f09119d3d0c6fe961e048b14bcb62f5
Dan Foley <dan@dan-foley.com>
2020-02-06 21:44:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   def kind
     sides = [@s1, @s2, @s3]
     binding.pry
-    if !(@s1 + @s2 > @s3) || sides.include?(0) || sides.all?(0) || sides.any?()
+    if !(@s1 + @s2 > @s3) || sides.include?(0) || sides.all?(0) || sides.any? {|s| s < 0}
       begin
         raise TriangleError
       rescue TriangleError => error
